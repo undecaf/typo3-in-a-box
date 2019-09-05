e5764f475c37bd967be2c4116273060e7774b176
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-09-05 09:19:22 +0200

    Fix merge defects

--- .travis/build/build.sh
+++ .travis/build/build.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-echo '*************** '"TRAVIS_BRANCH: '$TRAVIS_BRANCH'"
-echo '*************** '"TRAVIS_COMMIT: '$TRAVIS_COMMIT'"
-echo '*************** '"TRAVIS_TAG: '$TRAVIS_TAG'"
-echo '*************** '"TYPO3_VER: '$TYPO3_VER'"
-echo
+# Set environment variables common to all build stages
+source .travis/setenv.inc
 
-LOCAL_TAG=localhost/${TRAVIS_REPO_SLUG#*/}
-TEMP_TAG=$TRAVIS_REPO_SLUG:$TYPO3_VER-dev
+echo $'\n*************** '"Building $TEST_IMG"
 
 set -x
 
 docker build \
-    --pull \
-    --cache-from $TRAVIS_REPO_SLUG \
     --build-arg BUILD_DATE=$(date --utc +'%Y-%m-%dT%H:%M:%SZ') \
     --build-arg COMMIT=$TRAVIS_COMMIT \
     --build-arg TYPO3_VER=$TYPO3_VER \
     --build-arg IMAGE_VER=$IMAGE_VER \
-    --tag $LOCAL_TAG \
+    --tag $TEST_IMG \
     .
-
-docker tag $LOCAL_TAG $TEMP_TAG
