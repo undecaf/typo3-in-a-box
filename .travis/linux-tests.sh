6753f45a648ea2dee9dd6c15862a2f997f477210
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2019-09-12 23:32:11 +0200

    Test COMPOSER_EXCLUDE

--- .travis/linux-tests.sh
+++ .travis/linux-tests.sh
@@ -356,12 +356,12 @@ for D in ${EXCLUDED//:/ }; do
     grep -q -F $D $TEMP_FILE || echo "status = $?"
 done
 
-echo "Verifying that COMPOSER_EXCLUDE is being excluded"
-t3_ composer update >$TEMP_FILE
-for D in ${EXCLUDED//:/ }; do
-    grep -q -F "Excluded $D" $TEMP_FILE || echo "status = $?"
-    grep -q -F "Restored $D" $TEMP_FILE || echo "status = $?"
-done
+# echo "Verifying that COMPOSER_EXCLUDE is being excluded"
+# t3_ composer update >$TEMP_FILE
+# for D in ${EXCLUDED//:/ }; do
+#     grep -q -F "Excluded $D" $TEMP_FILE || echo "status = $?"
+#     grep -q -F "Restored $D" $TEMP_FILE || echo "status = $?"
+# done
 
 cleanup
 
