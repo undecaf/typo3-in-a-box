2a8ac5c25a910f389c910f18ce552330187b84bb
Ferdinand Kasper <ferdinand.kasper@gmx.at>
2020-06-01 22:59:40 +0200

    Build, test and deploy a sample image

--- .github/workflows/push.sh
+++ .github/workflows/push.sh
@@ -5,17 +5,16 @@ source .github/workflows/setenv.inc
 
 echo $'\n*************** '"Deploying $PRIMARY_IMG to $DEPLOY_TAGS"
 
-set -x
-
 # Tag primary image with all applicable tags and push them simultaneously
 for T in $DEPLOY_TAGS; do 
-    docker tag $PRIMARY_IMG $TRAVIS_REPO_SLUG:$T
+    docker tag $PRIMARY_IMG $GITHUB_REPOSITORY:$T
 done
 
 # Push all local tags
-docker login --username "$REGISTRY_USER" --password "$REGISTRY_PASS"
+docker login --username undecaf --password "$REGISTRY_PASS"
 docker push $TRAVIS_REPO_SLUG
 
 # Update bages at MicroBadger only for the most recent build version
-test -n "$MOST_RECENT" && \
-    curl -X POST https://hooks.microbadger.com/images/undecaf/typo3-in-a-box/$MICROBADGER_WEBHOOK
+test -n "$MOST_RECENT" \
+    && curl -X POST https://hooks.microbadger.com/images/undecaf/typo3-in-a-box/$MICROBADGER_WEBHOOK \
+    || true
